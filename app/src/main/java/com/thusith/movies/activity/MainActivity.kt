1c3aa14da6748eccca04168edfdc297f481df681
ThusithDev <anjulathusith@gmail.com>
2025-03-05 20:30:53 +0530

    Home Page and Search bar

--- app/src/main/java/com/thusith/movies/activity/MainActivity.kt
+++ app/src/main/java/com/thusith/movies/activity/MainActivity.kt
@@ -2,26 +2,40 @@ package com.thusith.movies.activity
 
 import android.os.Bundle
 import com.thusith.movies.activity.SplashActivity
+import com.thusith.movies.SearchBar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
@@ -45,13 +59,38 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(onItemClick:(FilmItemModel)->Unit={}){
 
-    Scaffold(bottomBar = { BottomNavigationBar() }
-    , floatingActionButton = {
+    Scaffold(bottomBar = { BottomNavigationBar() },
+
+        floatingActionButton = {
         Box(modifier = Modifier
             .size(60.dp)
-
-        )
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(colorResource(R.color.pink), colorResource(R.color.green))
+                ),
+                shape = CircleShape
+            )
+            .padding(3.dp)
+        ){
+            FloatingActionButton(
+                onClick = {},
+                containerColor = colorResource(id = R.color.black3),
+                modifier = Modifier.size(58.dp),
+                contentColor = Color.White,
+                content = {
+                    Icon(
+                        painter = painterResource(R.drawable.float_icon),
+                        contentDescription = null,
+                        modifier = Modifier.size(25.dp)
+                    )
+                }
+            )
         }
+        },
+        isFloatingActionButtonDocked = true,
+        floatingActionButtonPosition = FabPosition.Center,
+        backgroundColor = colorResource(R.color.blackBackground)
+
     ) { paddingValues ->
         Box(modifier = Modifier
             .padding(paddingValues)
@@ -66,6 +105,28 @@ fun MainScreen(onItemClick:(FilmItemModel)->Unit={}){
             )
         }
 
+        MainContent(onItemClick)
+
+    }
+}
+
+@Composable
+fun MainContent(onItemClick: (FilmItemModel) -> Unit) {
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .verticalScroll(rememberScrollState())
+        .padding(top = 60.dp, bottom = 100.dp)
+    ) {
+        Text(
+            text = "What would you like to watch?",
+            style = TextStyle(color = Color.White, fontSize = 25.sp),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, bottom = 16.dp)
+                .fillMaxWidth()
+        )
+    SearchBar(hint = "Search Movies...")
     }
 }
 
